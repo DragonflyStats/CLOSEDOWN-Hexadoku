9a0cfbb47aaa4c54590619285494381b2ee1c1b2
DragonflyStats <kobriendublin@gmail.com>
2016-06-04 16:30:46 +0100

    Create MCP4.r

---
+++ MCP4.r
@@ -0,0 +1,30 @@
+
+rows<-list( rep(CharSet),16) )
+
+paste("row",sprintf("%02.0f",1:16),sep="")-> names(rows)
+paste("col",sprintf("%02.0f",1:16),sep="")-> rames(cols)
+paste("grid",sprintf("%02.0f",1:16),sep="")-> names(grids)
+
+
+
+# InitialSetDifference
+####################################################################
+
+row01<-Charset; row02<-Charset; row03<-Charset; row04<-Charset
+row05<-Charset; row06<-Charset; row07<-Charset; row08<-Charset
+row09<-Charset; row10<-Charset; row11<-Charset; row12<-Charset
+row13<-Charset; row14<-Charset; row15<-Charset; row16<-Charset
+
+####################################################################
+col01<-Charset; col02<-Charset; col03<-Charset; col04<-Charset
+col05<-Charset; col06<-Charset; col07<-Charset; col08<-Charset
+col09<-Charset; col10<-Charset; col11<-Charset; col12<-Charset
+col13<-Charset; col14<-Charset; col15<-Charset; col16<-Charset
+####################################################################
+
+grid01<-Charset; grid02<-Charset; grid03<-Charset; grid04<-Charset; 
+grid05<-Charset; grid06<-Charset; grid07<-Charset; grid08<-Charset; 
+grid09<-Charset; grid10<-Charset; grid11<-Charset; grid12<-Charset; 
+grid13<-Charset; grid14<-Charset; grid15<-Charset; grid16<-Charset;
+
+####################################################################
